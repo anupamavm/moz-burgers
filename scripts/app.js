efee68f890d890dbea5d5a3f1b68d7f6298b2108
anupamavm <63632098+anupamavm@users.noreply.github.com>
2023-12-19 22:08:20 +0530

    food card done with dynamic usage

--- scripts/app.js
+++ scripts/app.js
@@ -19,12 +19,51 @@ const home = async () => {
 	const htmlContent = await fetchHtml('home.html');
 	app.innerHTML = htmlContent;
 };
-const food = async () => {
+
+const foods = async () => {
 	const htmlContent = await fetchHtml('food.html');
 	app.innerHTML = htmlContent;
 };
 
-const about = async () => {
-	const htmlContent = await fetchHtml('about.html');
+const customers = async () => {
+	const htmlContent = await fetchHtml('customers.html');
 	app.innerHTML = htmlContent;
 };
+
+const orders = async () => {
+	const htmlContent = await fetchHtml('orders.html');
+	app.innerHTML = htmlContent;
+};
+
+const reports = async () => {
+	const htmlContent = await fetchHtml('reports.html');
+	app.innerHTML = htmlContent;
+};
+
+// load the home to the app div when loading
+
+// // Function to execute when the #app div is added
+// function handleAppDiv(element) {
+// 	console.log('#app div added:', element);
+// 	home(); // Call the home function
+// }
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver((mutations) => {
+// 	mutations.forEach((mutation) => {
+// 		mutation.addedNodes.forEach((node) => {
+// 			if (node.nodeType === Node.ELEMENT_NODE && node.id === 'app') {
+// 				handleAppDiv(node);
+// 			}
+// 		});
+// 	});
+// });
+
+// // Start observing the target node for configured mutations
+// observer.observe(document.body, {
+// 	childList: true, // Set to true if you want to watch for additions/removals of child elements
+// 	subtree: true, // Set to true if you want to watch for changes in the entire subtree, not just direct children
+// });
+
+// // If you want to stop observing later
+// // observer.disconnect();
--- scripts/components/foodcard.js
+++ scripts/components/foodcard.js
@@ -5,27 +5,128 @@ class FoodCard extends HTMLElement {
 	}
 
 	connectedCallback() {
+		// Attributes
+		const imageUrl = this.getAttribute('image-src');
+		const priceValue = this.getAttribute('price') || '700';
+		const foodNameValue = this.getAttribute('food-name') || 'Classic Burger';
+		const subNameValue = this.getAttribute('sub-name') || '(Large)';
+
 		this.attachShadow({ mode: 'open' });
 		this.shadowRoot.innerHTML = `
 		<style>
-			/* Add your layout-specific styles here */
-			/* Use Bootstrap classes here */
-			.navbar {
-				background-color: #e3f2fd;
-			}
+		.card,
+		.card * {
+		  box-sizing: border-box;
+		}
+		.card {
+		  width: 166px;
+		  height: 284px;
+		  display:flex;
+		  margin:20px;
+		}
+		
 
-			.navbar h1 {
-				color: #2196f3;
-			}
+		.content {
+		  background: var(--gra, #eeeeee);
+		  border-radius: 20px;
+		  display:flex;
+		  flex-direction:column;
+		  width: 100%;
+		  height: 100%;
+		  align-items:center;
+		  position:relative;
+		  gap:20px;	
+		}
+		
+		.imageContainer {
+			top:6px;
+		  border-radius: 14px;
+		  height:150px;
+		  overflow: hidden;
+		  position: relative;   
+		}
 
-			.content-container {
-				padding: 20px;
-			}
-		</style>
-		<nav class="navbar">
-			<h1>Food Card</h1>
-		</nav>
-		<div class="content-container" id="content"></div>
+		img{
+			border:1px solid red;
+			height: 150px;  
+        width: 150px;  
+        object-fit: cover;
+		z-index:-1;
+		}
+		
+
+		.price {
+			position: absolute;       
+			bottom: 10px;            
+			background: rgba(0, 0, 0, 0.5);
+			z-index: 2;              
+			padding: 5px 10px;        
+			width:100%;
+			justify-content:center;
+			align-items:center;
+			color: white;     
+		}
+
+		.foodName{
+			
+			width:150px;
+			
+		}
+
+		.name{
+			font-size: 20px;
+font-weight: 400;
+		}
+
+		.addbtn{
+			padding: 10px 60px; 
+    		font-size: 16px;
+   			border: 2px solid black;
+    		border-radius: 15px; 
+    		background-color: none	; 
+    		color: balck; 
+    		cursor: pointer; 
+		}
+
+		.addbtn:hover{
+   			border: 2px solid #FF7A00;
+    		background-color: #FF7A00; 
+    		color: balck; 
+    		
+		}
+
+	</style>
+
+			
+<div class="card">
+  <div class="content">
+
+  <div class="imageContainer">
+  			<img  src="${imageUrl}"/>	
+
+			<div class="price">
+			  <span>
+				<span class="rs-span">Rs</span>
+				<span class="price-span">${priceValue}</span>
+			  </span>
+			</div>
+
+  </div>
+
+    <div class="foodName">
+      <span>
+        <span class="name">${foodNameValue}<br />
+        </span>
+        <span class="subName">${subNameValue}<br />
+        </span>
+      </span>        
+    </div>	
+
+	<button class="addbtn" onclick="">Add</button>
+
+
+  </div>
+</div>
 	`;
 		// Continue with your event listeners and navigation logic
 	}
